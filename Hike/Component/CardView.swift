c7441ed483298b1b6d0b00aacb752351917442e9
Melike2 <melikesoygul@hotmail.com>
2023-08-21 22:15:31 +0300

    second

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        
+        // MARK: CARD
         ZStack {
-            Circle()
-                .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading,
-                    endPoint: .bottomTrailing))
-            
-                .frame(width: 256, height: 256)
-            
-          Image("image-1")
-            .resizable()
-            .scaledToFit()
-            
-        }
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading,
+                        endPoint: .bottomTrailing))
+                
+                    .frame(width: 256, height: 256)
+                
+              Image("image-1")
+                .resizable()
+                .scaledToFit()
+                
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
