17b5fec3da40676880e8533fc15d600159e9ae0c
Melike2 <melikesoygul@hotmail.com>
2023-08-21 21:23:18 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Melike on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
