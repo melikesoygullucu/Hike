9a277c79355b5da2eccdd443a7bcb916a363bafb
Melike2 <melikesoygul@hotmail.com>
2023-08-22 10:48:26 +0300

    card view button was added

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -13,17 +13,49 @@ struct CardView: View {
         // MARK: CARD
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading,
-                        endPoint: .bottomTrailing))
+            VStack {
+                // MARK: - HEADER
+                VStack (alignment: .leading){
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                    startPoint: .top,
+                                endPoint: .bottom))
+                        
+                        Button{
+                            
+                        }label: {
+                            Text("Button")
+                        }
+                        
+                    }
+                    Text("Fun and enjoyable outdoor activities for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                    
+                } //: HEADER
+                .padding(.horizontal, 30)
                 
-                    .frame(width: 256, height: 256)
+                // MARK: - MAIN CONTENT
                 
-              Image("image-1")
-                .resizable()
-                .scaledToFit()
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading,
+                            endPoint: .bottomTrailing))
+                    
+                        .frame(width: 256, height: 256)
+                    
+                  Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+                }
                 
+                // MARK: - FOOTER
             }
         } //: CARD
         .frame(width: 320, height: 570)
